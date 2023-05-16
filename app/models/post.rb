c2b370beb86b851ed7cc951f44da700d005ec249
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-05-16 20:16:14 +0530

    Association Updated Fk: with user last three

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
-  has_many :comments, class_name: 'Comment', foreign_key: 'reference_id', dependent: :destroy
-  has_many :likes, class_name: 'Like', foreign_key: 'reference_id', dependent: :destroy
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments, class_name: 'Comment', foreign_key: :comment_id, dependent: :destroy
+  has_many :likes, class_name: 'Like', foreign_key: :like_id, dependent: :destroy
 end
