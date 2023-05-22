4b1fa0b4b2b7dc483b25d9be78f4e3702d4821f1
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-05-22 15:37:38 +0530

    New Posts feed route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    # @user = User.find(params[:id])
+  end
+  
   def show
     @post = Post.find(params[:id])
   end
