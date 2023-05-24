2aeec57144209b99dedd6658b212b684d4e4146d
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-05-24 22:12:42 +0530

    Rubocop fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
   def index
-      @user = User.find(params[:user_id])
-      @posts = @user.posts
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
-  
+
   def feed
     @posts = Post.all
   end
-  
+
   def show
     @post = Post.find(params[:id])
   end
