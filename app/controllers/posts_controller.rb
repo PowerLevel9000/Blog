72df93477c9f33cbd0e6abdcfe382f31dbbbe3ae
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-05-26 08:09:51 +0530

    Current user New Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,9 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'users#index' 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts
   end
   get 'feed', to: "posts#feed"
 end
