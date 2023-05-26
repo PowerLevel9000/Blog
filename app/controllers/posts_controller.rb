0e0e514860458e0e0c4902b8768fd5341e00bb9f
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-05-26 14:17:45 +0530

    CRUD Sucsses on Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,14 +29,29 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:id])
-  
+    @current_user = current_user
     if @post.destroy
-      redirect_to root_path, notice: "Post was successfully destroyed."
+      redirect_to user_posts_path(@current_user), notice: "Post was successfully destroyed."
     else
-      redirect_to root_path, alert: "Failed to destroy post."
+      redirect_to user_post_path(@current_user), alert: "Failed to destroy post."
     end
   end
   
+  def edit
+    @post = Post.find(params[:id])
+    @current_user = current_user
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @current_user = current_user
+    @post.update(
+      author: current_user,
+      title: post_params['title'],
+      text: post_params['text'],
+    )
+    redirect_to user_post_path(@current_user, @post), notice: "Post updated"
+  end
   
   private 
 
