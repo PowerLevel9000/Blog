963799218f85029f9b3d44e83498ef656f1164de
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-06-24 11:07:01 +0530

    Likes And Users End points

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [] do
-        resources :posts, only: [:index] do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show] do
           resources :comments, only: [:index, :create]
+          resources :likes, only: [:index, :create]
         end
       end
     end
