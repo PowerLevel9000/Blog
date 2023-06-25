12b0449e75822cba96d98bcd2b85f2e6f5f0af1c
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-06-25 08:15:00 +0530

    From Designing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+         :confirmable, :confirmable, :lockable, :trackable
 
   attribute :role, default: 'user'
   # Associations
